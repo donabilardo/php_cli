043bfe4d102e04df4e5bcfdbef61623ba8012ba8
unknown <don.abilardo@gmail.com>
2024-04-11 13:37:19 +0500

    cycle for

--- data/sem1/index.php
+++ data/sem1/index.php
@@ -1,7 +1,8 @@
 <?php
 
 for ($i = 0; $i <= 10; $i++) {
-    if ($i % 2 == 0) {
-        echo "Число $i четное" . PHP_EOL;
+    if ($i % 2 != 0) {
+        continue;
     }
+    echo "Число $i четное" . PHP_EOL;
 }
