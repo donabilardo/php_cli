db51bdfed77e2bb28835457a537fb5dda7555daa
unknown <don.abilardo@gmail.com>
2024-04-12 13:24:41 +0500

    тернарный оператор

--- data/sem1/ternar.php
+++ data/sem1/ternar.php
@@ -10,5 +10,9 @@ $age = readline("Сколько вам лет?" . PHP_EOL);
 
 $result = null;
 
+//после выражения ($a = 1) ставится ? если выражение истина то выполняется оно, иначе выполняется то что после :
+//(ВЫРАЖЕНИЕ) ? ЕСЛИ ИСТИНА : ЕСЛИ ЛОЖЬ
 ($age < 18) ? $result = "Вам меньше 18 лет" . PHP_EOL : $result = "Вам больше 18 лет" . PHP_EOL;
 echo $result;
+
+//docker compose run --rm -it cli php sem1/ternar.php
